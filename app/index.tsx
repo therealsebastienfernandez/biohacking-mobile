acc18728187b19fa37f754d8e206f55d8f56293e
sebastienfernandez <sebastienfernandez@protonmail.com>
2026-02-23 23:02:08 +0100

    change the entry words

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Votre application mobile de biohacking personnalisé</Text>
     </View>
   );
 }
